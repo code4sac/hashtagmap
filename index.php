06b887636794fdc36d9f8a603035a83ac304425a
Kaleb Clark <kaleb@abraxxus.net>
2013-09-25 19:28:10 -0700

    env setup

--- index.php
+++ index.php
@@ -1,58 +1,13 @@
-<?php
-ini_set('display_errors', 1);
-require_once('lib/TwitterAPIExchange.php');
-require_once('lib/util.php');
+<HTML>
+<head>
+  <script type="text/javascript" src="javascript/emerge.js"></script>
+  <script type="text/javascript" src="javascript/vendor/jquery-2.0.3.min.js"></script>
+</head>
+<body>
 
-$hash_tag = "#cfabrigade";
+<div id="dumper"></div>
+<script type="text/javascript">
 
-/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
-$settings = array(
-    'oauth_access_token' => "284255296-KHR2Uv8Fw2A9UQZUGpsLMl2wLc2NVbd0iy1aNsSL",
-    'oauth_access_token_secret' => "nwcwFRyMjRMAYmlcDCz8MjD0AIKS0oF0BWnherUIiI",
-    'consumer_key' => "Dr0QqWsJhcqO9lcnn1clxg",
-    'consumer_secret' => "SLPoBbkoPbbN4n9PUgZDMo7catu7XQyZAJ4DP8Sd6AQ"
-);
-
-/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
-//$url = 'https://api.twitter.com/1.1/blocks/create.json';
-//$requestMethod = 'POST';
-
-/** POST fields required by the URL above. See relevant docs as above **/
-//$postfields = array(
-//    'screen_name' => 'usernameToBlock', 
-//    'skip_status' => '1'
-//);
-
-/** Perform a POST request and echo the response **/
-//$twitter = new TwitterAPIExchange($settings);
-//echo $twitter->buildOauth($url, $requestMethod)
-//             ->setPostfields($postfields)
-//             ->performRequest();
-$url = 'https://api.twitter.com/1.1/search/tweets.json';
-$getfield = '?q='.$hash_tag.'&include_entities=true';
-$requestMethod = 'GET';
-$twitter = new TwitterAPIExchange($settings);
-$ret = $twitter->setGetfield($getfield)
-             ->buildOauth($url, $requestMethod)
-             ->performRequest();
-
-print $ret;
-
-exit;
-
-
-
-
-
-/** Perform a GET request and echo the response **/
-/** Note: Set the GET field BEFORE calling buildOauth(); **/
-$url = 'https://api.twitter.com/1.1/followers/ids.json';
-$getfield = '?screen_name=_abraxxus';
-$requestMethod = 'GET';
-$twitter = new TwitterAPIExchange($settings);
-echo $twitter->setGetfield($getfield)
-             ->buildOauth($url, $requestMethod)
-             ->performRequest();
-
-
-
+</script>
+</body>
+</html>
